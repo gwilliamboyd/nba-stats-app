7bc5e5d4c4da1b8fdfc80e38ee6301efab6831ad
G William Boyd <gwillboyd@gmail.com>
2023-07-03 16:33:18 -0500

    mongoose.connect now uses string instead of env

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,13 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI)
+		const conn = await mongoose.connect(
+			'mongodb+srv://gwillboyd:TqRUS54bbGB81Vxo@stats.q8smoe6.mongodb.net/stats?retryWrites=true&w=majority',
+			{
+				useNewUrlParser: 'true',
+				useUnifiedTopology: 'true',
+			}
+		)
 		console.log(`MongoDB connected: ${conn.connection.host}`)
 	} catch (err) {
 		console.error(`Error ${err.message}`)
