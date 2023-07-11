dfd0f502fa953af142cf76e017ef68690167d091
G William Boyd <gwillboyd@gmail.com>
2023-07-11 12:15:04 -0500

    Multer dir change

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 // Image upload
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, process.cwd() + '/uploads/')
+		cb(null, './server/public/assets')
 	},
 	filename: function (req, file, cb) {
 		cb(null, file.originalname)
